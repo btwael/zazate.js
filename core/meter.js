39ef83f90bed71094a1e8de457970ccd00f6521c
Wael Amine Boutglay <btwael@gmail.com>
2014-01-01 16:24:07 +0000

    Add meter.js

---
+++ core/meter.js
@@ -0,0 +1,41 @@
+common_time = (4, 4);
+cut_time = (2, 2);
+
+function valid_beat_duration(duration) {
+	if(duration == 0) {
+		return false;
+	} else if(duration == 1) {
+		return true;
+	} else {
+		r = duration;
+		while(r != 1) {
+			if(r % 2 == 1) {
+				return false;
+			}
+			r /= 2;
+		}
+		return true;
+	}
+}
+
+function is_valid(meter) {
+	return meter[0] > 0 && valid_beat_duration(meter[1]);
+}
+
+function is_compound(meter) {
+	return is_valid(meter) && meter[0] % 3 == 0;
+}
+
+function is_simple(meter) {
+	return is_valid(meter);
+}
+
+function is_asymmetrical(meter) {
+	return is_valid(meter) && meter[0] % 2 == 1;
+}
+//export
+exports.valid_beat_duration = valid_beat_duration;
+exports.is_valid = is_valid;
+exports.is_compound = is_compound;
+exports.is_asymmetrical = is_asymmetrical;
+exports.is_simple = is_simple;
