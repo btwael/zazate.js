21e750d443ff24df446b6ba335bb4ec528dcc6ca
Wael Boutglay <btwael@gmail.com>
2014-09-22 12:03:07 +0000

    Some test for 'values'

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var should = require('should'),
 	zazate = require('../core/init.js');
+
 // Test for meter
 describe('Meter', function(){
 	var meter = zazate.meter,
@@ -53,7 +54,7 @@ describe('Meter', function(){
 		})
 	});
 	describe('#is_asymmetrical()', function(){
-		it('returns true if meter is an asymmetrical meter, false otherwise.', function(){
+		it('should return true if meter is an asymmetrical meter, false otherwise.', function(){
 			for (var i = 0; i < asymmetrical_meters.length; i++) {
 				meter.is_asymmetrical(asymmetrical_meters[i]).should.eql(true);
 			};
@@ -61,7 +62,7 @@ describe('Meter', function(){
 		})
 	});
 	describe('#is_compound()', function(){
-		it('returns true if meter is a compund meter, false otherwise.', function(){
+		it('should return true if meter is a compund meter, false otherwise.', function(){
 			for (var i = 0; i < compound_meters.length; i++) {
 				meter.is_compound(compound_meters[i]).should.eql(true);
 			};
@@ -69,14 +70,14 @@ describe('Meter', function(){
 		})
 	});
 	describe('#is_simple()', function(){
-		it('returns true if meter is a simple meter, false otherwise.', function(){
+		it('should return true if meter is a simple meter, false otherwise.', function(){
 			for (var i = 0; i < simple_meters.length; i++) {
 				meter.is_simple(simple_meters[i]).should.eql(true);
 			};
 		})
 	});
 	describe('#is_valid()', function(){
-		it('returns true if meter is a valid array representation of a meter', function(){
+		it('should return true if meter is a valid array representation of a meter.', function(){
 			for (var i = 0; i < simple_meters.length; i++) {
 				meter.is_valid(simple_meters[i]).should.eql(true);
 			};
@@ -85,4 +86,44 @@ describe('Meter', function(){
 			};
 		})
 	});
-})
+});
+
+// Test for value
+describe('Value', function(){
+	var value = zazate.value;
+	describe('#base_quintuplets', function(){
+		it('should equal [0.3125, 0.625, 1.25, 2.5, 5, 10, 20, 40, 80, 160].', function(){
+			value.base_quintuplets.should.eql([0.3125, 0.625, 1.25, 2.5, 5, 10, 20, 40, 80, 160]);
+		})
+	});
+	describe('#base_septuplets', function(){
+		it('should equal [0.4375, 0.875, 1.75, 3.5, 7, 14, 28, 56, 112, 224].', function(){
+			value.base_septuplets.should.eql([0.4375, 0.875, 1.75, 3.5, 7, 14, 28, 56, 112, 224]);
+		})
+	});
+	describe('#base_triplets', function(){
+		it('should equal [0.375, 0.75, 1.5, 3, 6, 12, 24, 48, 96, 192].', function(){
+			value.base_triplets.should.eql([0.375, 0.75, 1.5, 3, 6, 12, 24, 48, 96, 192]);
+		})
+	});
+	describe('#base_values', function(){
+		it('should equal [0.25, 0.5, 1, 2, 4, 8, 16, 32, 64, 128].', function(){
+			value.base_values.should.eql([0.25, 0.5, 1, 2, 4, 8, 16, 32, 64, 128]);
+		})
+	});
+	describe('#breve', function(){
+		it('should equal 0.5.', function(){
+			value.breve.should.eql(0.5);
+		})
+	});
+	describe('#crotchet', function(){
+		it('should equal 4.', function(){
+			value.crotchet.should.eql(4);
+		})
+	});
+	describe('#demisemiquaver', function(){
+		it('should equal 32.', function(){
+			value.demisemiquaver.should.eql(32);
+		})
+	});
+});
